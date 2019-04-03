735984cf0025760cdb6893b88c2a2da83b6321ec
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-03 01:38:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,9 @@ end
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
   calls.each do |call|
-    if call.length <= 5
+    if call.length >= 5
       return true
-    else
+    elsif call.length <= 4
       return false
     end
   end
