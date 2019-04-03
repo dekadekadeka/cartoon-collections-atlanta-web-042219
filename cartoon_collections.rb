1e59c656e6b3c89df932622a65b4634a3563a0c7
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-03 00:54:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves(names)# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index{|names, index| puts "#{index}. #{names}"}
 end
 
 def summon_captain_planet# code an argument here
