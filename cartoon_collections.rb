3d4a214a10f6d9936f0e8bf6df3a97a7c68a9f6a
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-03 01:18:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  calls.each {|call| if call.length > 4 return true}
+  calls.each {|call| if call.length > 4 return true else false end}
 end
 
 def find_the_cheese# code an argument here
