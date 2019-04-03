21e54620bd97c3d93f2aa9ab5c89460a5f6034d6
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-03 00:58:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(planeteer)# code an argument here
   # Your code here
-  planeteer.collect{|name| name + "!"}
+  planeteer.collect{|name| name.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
